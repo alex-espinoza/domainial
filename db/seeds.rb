f64f8689f6c3bb30e68b896002aceaf53984c8b9
Alex Espinoza <aespinoza@aspria.net>
2016-09-27 18:48:56 -0400

    Seed some wanted domain data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+WantedDomain.create(name: 'portal', tld: '.io')
+WantedDomain.create(name: 'tether', tld: '.io')
