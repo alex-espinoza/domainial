9174640cef6cb3d55fd50e0a2360906d3ce20fe2
Alex Espinoza <aespinoza@aspria.net>
2016-09-28 01:46:01 -0400

    Create functionality to add new wanted domains via form

--- app/controllers/wanted_domains_controller.rb
+++ app/controllers/wanted_domains_controller.rb
@@ -2,4 +2,25 @@ class WantedDomainsController < ApplicationController
   def index
     @wanted_domains = WantedDomain.all
   end
+
+  def new
+    @wanted_domain = WantedDomain.new
+  end
+
+  def create
+    @wanted_domain = WantedDomain.new(wanted_domain_params)
+    @wanted_domain.tld = '.io'
+
+    if @wanted_domain.valid? && @wanted_domain.save
+      redirect_to root_path
+    else
+      render 'create.html'
+    end
+  end
+
+  private
+
+  def wanted_domain_params
+    params.require(:wanted_domain).permit(:name)
+  end
 end
