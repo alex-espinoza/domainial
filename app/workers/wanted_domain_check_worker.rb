12ed0c0608c255416480250060de04c0a01e0448
Alex Espinoza <aespinoza@aspria.net>
2016-10-01 16:36:08 -0400

    Update proxies and more settings on wanted domain worker

--- app/workers/wanted_domain_check_worker.rb
+++ app/workers/wanted_domain_check_worker.rb
@@ -5,8 +5,7 @@ class WantedDomainCheckWorker
   shoryuken_options queue: ENV['WANTED_DOMAIN_CHECK_AVAILABILITY'],
                     auto_delete: true,
                     body_parser: :json,
-                    retry_intervals: 120,
-                    auto_visibility_timeout: true
+                    retry_intervals: 90
 
   def perform(sqs_message, data)
     domain = WantedDomain.find(data['domain_id'])
@@ -27,41 +26,32 @@ class WantedDomainCheckWorker
 
   def proxy_list
     [
+      ["166.70.157.58", "80"],
+      ["173.161.0.227", "80"],
+      ["45.32.214.48",  "808"],
+      ["52.37.174.68",  "80"],
+      ["97.107.129.183",  "80"],
+      ["104.236.54.196",  "8080"],
+      ["104.247.206.88",  "3128"],
+      ["184.49.233.234",  "8080"],
+      ["68.71.156.223", "8800"],
+      ["68.71.156.115", "8800"],
+      ["68.71.148.86",  "8800"],
+      ["172.82.132.117",  "8800"],
+      ["198.46.224.49", "8800"],
+      ["23.89.42.159",  "8800"],
+      ["172.82.178.119",  "8800"],
+      ["23.89.41.178",  "8800"],
+      ["97.77.104.22",  "80"],
       ["159.203.111.101", "3128"],
-      ["166.70.157.58", "80"],
       ["138.68.152.51", "8080"],
-      ["209.64.81.18", " 8080"],
-      ["209.150.253.82", " 80"],
-      ["192.169.174.121", "8088"],
-      ["199.116.113.206", "8080"],
-      ["162.209.1.221", "8080"],
-      ["173.161.0.227", "80"],
-      ["97.77.104.22", "80"],
-      ["173.161.0.227", "80"],
-      ["166.70.157.58", "80"],
-      ["74.202.77.224", "80"],
-      ["45.32.194.8", "8080"],
-      ["52.43.19.20", "80"],
-      ["169.50.87.252", "80"],
-      ["208.40.165.200", "80"],
-      ["45.32.61.250", "3128"],
-      ["104.247.206.243", "3128"],
-      ["184.75.243.101", "13100"],
-      ["45.32.218.222", "808"],
-      ["52.30.138.5", "3128"],
-      ["45.55.143.243", "3128"],
-      ["45.32.212.133", "808"],
-      ["69.51.111.14" , "80"],
-      ["52.23.236.195", "80"],
-      ["69.50.56.73", "8080"],
-      ["184.49.233.234", "8080"],
-      ["107.170.213.149", "3128"]
+      ["209.64.81.18",  "8080"],
     ]
   end
 
   def make_http_request(domain)
     random_proxy = proxy_list.sample
-    request_options = {timeout: 10, http_proxyaddr: random_proxy[0], http_proxyport: random_proxy[1]}
+    request_options = {timeout: 15, http_proxyaddr: random_proxy[0], http_proxyport: random_proxy[1]}
 
     response = self.class.get("/#{domain.name_with_tld}", request_options)
   end
@@ -82,8 +72,8 @@ class WantedDomainCheckWorker
 
   def save_as_available(domain)
     domain.update(checked?: 1,
-                   status: 'Available',
-                   status_code: 1)
+                  status: 'Available',
+                  status_code: 1)
   end
 
   def save_as_not_available(page_content, domain)
@@ -108,14 +98,14 @@ class WantedDomainCheckWorker
     end
 
     domain.update(checked?: 1,
-                   status: status,
-                   status_code: 0,
-                   first_registered_date: first_registered_date,
-                   last_updated_date: last_updated_date,
-                   expiry_date: expiry_date,
-                   grace_period_ends_date: grace_period_ends_date,
-                   backorder: backorder,
-                   owner_name: owner_name,
-                   organization_name: organization_name)
+                  status: status,
+                  status_code: 0,
+                  first_registered_date: first_registered_date,
+                  last_updated_date: last_updated_date,
+                  expiry_date: expiry_date,
+                  grace_period_ends_date: grace_period_ends_date,
+                  backorder: backorder,
+                  owner_name: owner_name,
+                  organization_name: organization_name)
   end
 end
