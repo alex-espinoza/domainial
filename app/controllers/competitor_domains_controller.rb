594161ebfb8c6fee281da3eb2cc9eefb72d16d2b
Alex Espinoza <aespinoza@aspria.net>
2016-10-10 10:57:32 -0400

    Show last 50 domains sold on competitor domains index page

--- app/controllers/competitor_domains_controller.rb
+++ app/controllers/competitor_domains_controller.rb
@@ -1,5 +1,6 @@
 class CompetitorDomainsController < ApplicationController
   def index
     @revenue_by_tld = CompetitorDomain.group(:tld).sum(:price).sort_by {|tld, price| price}.reverse
+    @last_30_domains_sold = CompetitorDomain.where(active: 0).order(sold_date: :desc).limit(50)
   end
 end
--- app/models/competitor_domain.rb
+++ app/models/competitor_domain.rb
@@ -1,4 +1,12 @@
 class CompetitorDomain < ApplicationRecord
   # active can have 2 states: 0 = not active (sold/auction ended), 1 = auction ongoing
   validates :name, :tld, :price, :source, :active, presence: true
+
+  def name_with_tld
+    "#{self.name}.#{self.tld}"
+  end
+
+  def pretty_date(datetime)
+    (datetime && datetime.to_formatted_s(:long)) || ''
+  end
 end
