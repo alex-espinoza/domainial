b3a98ceb7b36f299efe8aafd4105abca0d6da17c
Alex Espinoza <aespinoza@aspria.net>
2016-10-10 20:08:54 -0400

    Update ExpiredDomainsPendingDeleteScraperWorker to also check txt file for .to domains

--- app/workers/expired_domains_pending_delete_scraper_worker.rb
+++ app/workers/expired_domains_pending_delete_scraper_worker.rb
@@ -1,18 +1,26 @@
 class ExpiredDomainsPendingDeleteScraperWorker
   def initialize
     @login_url = 'https://www.expireddomains.net/login/'
-    @pending_delete_domain_txt_file_url = 'https://member.expireddomains.net/export/pendingdelete/?export=textfile&flast30d=1&ftlds[]=125&flimit=25&fconsephost=1&fsephost=1&o=enddate&r=a'
+    @io_file_url = 'https://member.expireddomains.net/export/pendingdelete/?export=textfile&flast30d=1&ftlds[]=125&flimit=25&fconsephost=1&fsephost=1&o=enddate&r=a'
+    @to_file_url = 'https://member.expireddomains.net/export/pendingdelete/?export=textfile&activetab=expireddomains&flast30d=1&ftlds[]=238&flimit=25&fsephost=1&o=enddate&r=a'
+    @domain_file_urls = [@io_file_url, @to_file_url]
     @username = 'cyant'
     @password = 'Ildeexpired90'
     @agent = Mechanize.new
+    @current_file_url = nil
     @txt_filename = nil
   end
 
   def perform
     log_in_to_site
-    download_pending_delete_domains_text_file
-    parse_text_file
-    delete_file
+
+    @domain_file_urls.each do |file_url|
+      @current_file_url = file_url
+
+      download_pending_delete_domains_text_file
+      parse_text_file
+      delete_file
+    end
   end
 
   def log_in_to_site
@@ -24,7 +32,7 @@ class ExpiredDomainsPendingDeleteScraperWorker
   end
 
   def download_pending_delete_domains_text_file
-    file = @agent.get(@pending_delete_domain_txt_file_url)
+    file = @agent.get(@current_file_url)
     @txt_filename = file.save("tmp/txt/#{file.filename}")
   end
 
@@ -32,10 +40,7 @@ class ExpiredDomainsPendingDeleteScraperWorker
     # do not load whole file into memory, read line by line
     File.foreach(@txt_filename) do |line|
       domain_array = line.strip.split('.')
-
-      if domain_array[1] == 'io'
-        save_domain_as_wanted(domain_array)
-      end
+      save_domain_as_wanted(domain_array)
     end
   end
 
