ee7157d3aee9aa6efb0864dfca0e201d5fef1f6b
Alex Espinoza <aespinoza@aspria.net>
2016-10-10 23:06:16 -0400

    Update drop check to only check .io domains for now

--- app/models/drop_check.rb
+++ app/models/drop_check.rb
@@ -3,7 +3,7 @@ class DropCheck < ApplicationRecord
 
   def self.check_availability_on_drop
     todays_io_drop_time = WantedDomain.io_drop_time
-    domains_to_be_checked = WantedDomain.where(grace_period_ends_date: todays_io_drop_time, interested?: 1)
+    domains_to_be_checked = WantedDomain.where(tld: 'io', grace_period_ends_date: todays_io_drop_time, interested?: 1)
 
     domains_to_be_checked.each do |domain|
       DropCheckWorker.perform_async(domain.id)
